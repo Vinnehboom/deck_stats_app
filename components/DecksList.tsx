500e3cc703acd276eabab0e9f6944a6efb43cf58
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2023-08-19 16:22:55 +0200

    Setup Firebase Database config

--- components/DecksList.tsx
+++ components/DecksList.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { colors } from '../utils/colors'
+import {Text, View, StyleSheet} from 'react-native';
+import {colors} from '../utils/colors';
 
-export const DecksList = ({ decks }) => {
+export const DecksList = ({decks}) => {
   return (
     <View style={styles.background}>
       <Text style={title}> Active Decks </Text>
@@ -18,5 +18,5 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     color: colors.red,
-  }
+  },
 });
