4e581f94909812e84899b2b61de9ef1f130ec4bf
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2023-08-19 14:50:27 +0200

    Initial git state

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { colors } from '../utils/colors'
+
+export const Header = () => {
+  return (
+    <View style={styles.background}>
+      <Text style={styles.title}>
+        Deck Stats TCG
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: colors.darkBlue,
+    padding: 24
+  },
+  title: {
+    color: colors.white,
+    fontSize: 32,
+    textAlign: 'center'
+  }
+})
