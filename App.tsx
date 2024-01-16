ff6ada1535d1b3e4c9c785ee5ea5f2ce75381b75
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-01-16 20:05:13 +0100

    Install tanstack and add query client to App.tsx

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NativeBaseProvider } from "native-base";
 import FlashMessage from "react-native-flash-message";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { LoginScreen } from "./screens/LoginScreen";
 import { HomeScreen } from "./screens/HomeScreen";
@@ -10,18 +11,20 @@ import { DecklistHome } from "./screens/decklists/decklist/DecklistHome";
 import { RootStackParamList } from "./types/RouteParams";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
-
+const queryClient = new QueryClient();
 export function App(): JSX.Element {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-          <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
-          <Stack.Screen name="DecklistHome" options={{ animation: "fade", headerShown: false }} component={DecklistHome} />
-        </Stack.Navigator>
-        <FlashMessage position="top" />
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <QueryClientProvider client={queryClient}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+            <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
+            <Stack.Screen name="DecklistHome" options={{ animation: "fade", headerShown: false }} component={DecklistHome} />
+          </Stack.Navigator>
+          <FlashMessage position="top" />
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </QueryClientProvider>
   );
 }
