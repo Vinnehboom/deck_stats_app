fbf44c2ab3af03fa8fe86ef37a2145f648dc2ec6
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-02-23 17:20:40 +0100

    Add typing to infer translation keys using `t()`

---
+++ @types/resources.ts
@@ -0,0 +1,8 @@
+import translation from "../locales/en/translation.json";
+
+const resources = {
+  translation,
+} as const;
+
+// eslint-disable-next-line import/no-default-export
+export default resources;
