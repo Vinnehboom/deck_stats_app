b7f9f275e33739a3626c5dedc4f2b4b1082b9b4b
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-02-23 17:20:40 +0100

    Add typing to infer translation keys using `t()`

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -1,21 +1,22 @@
-import i18n from "i18next";
+import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 
 // Import your language files here
-import { en } from "../locales";
+import { en } from "../locales/";
 
 // Set up i18next
-i18n.use(initReactI18next).init({
-  lng: "en", // Set the default language here
+i18next.use(initReactI18next).init({
+  debug: true,
   fallbackLng: "en", // Set the fallback language here
+  returnNull: false,
   interpolation: {
     escapeValue: false, // not needed for react!!
   },
   resources: {
     en: {
-      translation: en, // Add your English translations here
+      translation: en,
     },
   },
 });
 
-export { i18n };
+export { i18next };
