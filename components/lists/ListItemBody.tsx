ec37369412322db647cb64080b32fff238df5c5a
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-03-09 11:59:08 +0100

    Rework ListItem component to multiple reusable components

---
+++ components/lists/ListItemBody.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { HStack, View, Text, Box, Button, Center } from "native-base";
+import { useTranslation } from "react-i18next";
+
+import { CardListImage } from "./cardLists/CardListImage";
+import { List, CardList, CardListItem } from "../../types";
+import { ListItemStyle } from "../../styles/lists/ListItemStyle";
+
+export const ListItemBody = ({ list }: { list: List }) => {
+  const cards = list?.cards;
+  const [display, setDisplay] = useState<"list" | "image">("list");
+  const { t } = useTranslation();
+
+  const renderImages = ({ cardList, columnLength }: { cardList: CardList; columnLength: number }) => {
+    let column: CardListItem[] = [];
+    const imageColumns: CardListItem[][] = [];
+
+    cardList.forEach((cardListItem: CardListItem) => {
+      column.push(cardListItem);
+      if (column.length === columnLength) {
+        imageColumns.push(column);
+        column = [];
+      }
+    });
+    return (
+      <View style={ListItemStyle.imageContainer}>
+        {imageColumns.map((kolumn, count) => (
+          <HStack margin={1} space={1} key={count}>
+            {kolumn.map((cardListItem, kount) => (
+              <CardListImage key={kount} count={cardListItem.count} card={cardListItem.card} />
+            ))}
+          </HStack>
+        ))}
+      </View>
+    );
+  };
+
+  const renderList = (cardList: CardList) => {
+    const renderedList: React.JSX.Element[] = cardList.map((cardListItem, index) => {
+      const { count, card } = cardListItem;
+      return (
+        <Box key={index}>
+          <HStack style={ListItemStyle.listLine}>
+            <Text style={ListItemStyle.listLineCount}>{count}</Text>
+            <Text style={ListItemStyle.listLineName}>{card.name}</Text>
+            <Text style={ListItemStyle.listLineSet}>
+              {card.setId} {card.setNumber}
+            </Text>
+          </HStack>
+        </Box>
+      );
+    });
+    return renderedList;
+  };
+
+  return (
+    <Box style={ListItemStyle.item}>
+      <HStack>
+        <Box padding={4} minWidth="50%">
+          <Button
+            onPress={() => setDisplay("list")}
+            style={display === "list" ? ListItemStyle.activeTabButton : ListItemStyle.inactiveTabButton}>
+            <Center>
+              <Text style={display === "list" ? ListItemStyle.activeTabButtonText : ListItemStyle.inactiveTabButtonText}>
+                {t("LIST.ITEM.LIST")}
+              </Text>
+            </Center>
+          </Button>
+        </Box>
+        <Box padding={4} minWidth="50%">
+          <Button
+            onPress={() => setDisplay("image")}
+            style={display === "image" ? ListItemStyle.activeTabButton : ListItemStyle.inactiveTabButton}>
+            <Center>
+              <Text style={display === "image" ? ListItemStyle.activeTabButtonText : ListItemStyle.inactiveTabButtonText}>
+                {t("LIST.ITEM.IMAGES")}
+              </Text>
+            </Center>
+          </Button>
+        </Box>
+      </HStack>
+
+      {display === "image" ? renderImages({ cardList: cards, columnLength: 4 }) : renderList(cards)}
+    </Box>
+  );
+};
---
+++ components/lists/ListItemHeader.tsx
@@ -0,0 +1,40 @@
+import { Box, HStack, Text, Button } from "native-base";
+import React from "react";
+import { showMessage } from "react-native-flash-message";
+import { useTranslation } from "react-i18next";
+
+import { useSetActiveList } from "./_queries/useSetActiveList";
+import { ListItemStyle } from "../../styles/lists/ListItemStyle";
+import { Deck, List } from "../../types";
+
+export const ListItemHeader = ({ list, deck, activeListId }: { list: List; deck: Deck; activeListId: List["id"] }) => {
+  const { t } = useTranslation();
+  const listActivationMutation = useSetActiveList(deck, () => {
+    showMessage({
+      message: t("DECK.DECK_DETAILS.ACTIVE_DECK.SUCCESS"),
+      type: "info",
+    });
+  });
+  return (
+    <HStack style={ListItemStyle.header}>
+      <Text style={ListItemStyle.titlePaddingBox}>&nbsp;</Text>
+      <Box style={ListItemStyle.titleBox}>
+        <Text style={ListItemStyle.title}>{list.name}</Text>
+      </Box>
+
+      <Box style={ListItemStyle.titleActionBox}>
+        {activeListId === list.id ? (
+          <Text>Active List</Text>
+        ) : (
+          <Button
+            style={ListItemStyle.activateButton}
+            onPress={() => {
+              listActivationMutation.mutate(list);
+            }}>
+            <Text style={ListItemStyle.activateButtonText}>Set active</Text>
+          </Button>
+        )}
+      </Box>
+    </HStack>
+  );
+};
