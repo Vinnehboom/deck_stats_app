11ac687b39912314e494adea142a942f4eb8b915
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-03-20 18:09:38 +0100

    Rework register to ask for password confirmation
    also changed state logic to a reducer

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import { TextInput, View } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 import { showMessage } from "react-native-flash-message";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useTranslation } from "react-i18next";
-import { Text, Box, Button } from "native-base";
+import { Text, Box, Button, Link } from "native-base";
 import { StrokeText } from "@charmy.tech/react-native-stroke-text";
 
 import { authInstance } from "../firebase/firebaseconfig";
@@ -14,9 +14,37 @@ import { LoginScreenContainer } from "../components/layout/LoginScreenContainer"
 import { LoginScreenStyle } from "../styles/login/LoginScreenStyle";
 import { TermsAndConditions } from "../components/layout/TermsAndConditions";
 
+type loginStateType = { email: string; password: string; passwordConfirmation: string };
+
+const loginFormReducer = (
+  state: loginStateType,
+  action: { type: "SET_EMAIL" | "SET_PASSWORD" | "SET_PASSWORD_CONFIRMATION" | "CLEAR"; payload?: string }
+): loginStateType => {
+  switch (action.type) {
+    case "SET_EMAIL": {
+      return typeof action.payload === "string" ? { ...state, email: action.payload } : { ...state };
+    }
+    case "SET_PASSWORD": {
+      return typeof action.payload === "string" ? { ...state, password: action.payload } : { ...state };
+    }
+
+    case "SET_PASSWORD_CONFIRMATION": {
+      return typeof action.payload === "string" ? { ...state, passwordConfirmation: action.payload } : { ...state };
+    }
+
+    case "SET_EMAIL": {
+      return typeof action.payload === "string" ? { ...state, email: action.payload } : { ...state };
+    }
+    case "CLEAR": {
+      return { email: "", password: "", passwordConfirmation: "" };
+    }
+  }
+};
+
 export const LoginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [loginState, loginFormDispatch] = useReducer(loginFormReducer, { email: "", password: "", passwordConfirmation: "" });
+
+  const [login, setLogin] = useState(true);
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   const { t } = useTranslation();
 
@@ -31,16 +59,22 @@ export const LoginScreen = () => {
   }, [navigation]);
 
   const handleSignUp = () => {
-    if (email.trim() === "" || password.trim() === "") {
-      showMessage({
+    if (loginState.email.trim().length < 1 || loginState.password.trim().length < 1) {
+      return showMessage({
         message: t("LOGIN_SCREEN.INCOMPLETE_FORM"),
         type: "warning",
       });
-      return;
+    }
+
+    if (loginState.password !== loginState.passwordConfirmation) {
+      return showMessage({
+        message: t("LOGIN_SCREEN.PASSWORD_MISMATCH"),
+        type: "warning",
+      });
     }
 
     authInstance
-      .createUserWithEmailAndPassword(email, password)
+      .createUserWithEmailAndPassword(loginState.email, loginState.password)
       .then(() => {
         showMessage({
           message: t("LOGIN_SCREEN.SIGN_UP.SUCCESS"),
@@ -56,7 +90,7 @@ export const LoginScreen = () => {
   };
 
   const handleLogin = () => {
-    if (email.trim() === "" || password.trim() === "") {
+    if (loginState.email.trim().length < 1 || loginState.password.trim().length < 1) {
       showMessage({
         message: t("LOGIN_SCREEN.INCOMPLETE_FORM"),
         type: "warning",
@@ -65,7 +99,7 @@ export const LoginScreen = () => {
     }
 
     authInstance
-      .signInWithEmailAndPassword(email, password)
+      .signInWithEmailAndPassword(loginState.email, loginState.password)
       .then(() => {
         showMessage({
           message: t("LOGIN_SCREEN.SIGN_IN.SUCCESS"),
@@ -102,32 +136,53 @@ export const LoginScreen = () => {
                 strokeColor={colors["primary-dark"]}
                 strokeWidth={3}
                 fontSize={26}
-                text={t("LOGIN_SCREEN.FORM.LOGIN_TITLE")}
+                text={login ? t("LOGIN_SCREEN.FORM.LOGIN_TITLE") : t("LOGIN_SCREEN.FORM.REGISTER_TITLE")}
               />
             </Box>
             <TextInput
               placeholder={t("LOGIN_SCREEN.FORM.EMAIL")}
-              value={email}
-              onChangeText={text => setEmail(text)}
+              value={loginState.email}
+              onChangeText={text => loginFormDispatch({ type: "SET_EMAIL", payload: text })}
               style={LoginScreenStyle.input}
             />
             <TextInput
               placeholder={t("LOGIN_SCREEN.FORM.PASSWORD")}
-              value={password}
-              onChangeText={text => setPassword(text)}
+              value={loginState.password}
+              onChangeText={text => loginFormDispatch({ type: "SET_PASSWORD", payload: text })}
               style={LoginScreenStyle.input}
               secureTextEntry
             />
+            {login ? null : (
+              <TextInput
+                placeholder={t("LOGIN_SCREEN.FORM.PASSWORD_CONFIRMATION")}
+                value={loginState.passwordConfirmation}
+                onChangeText={text => loginFormDispatch({ type: "SET_PASSWORD_CONFIRMATION", payload: text })}
+                style={LoginScreenStyle.input}
+                secureTextEntry
+              />
+            )}
           </View>
 
           <View style={LoginScreenStyle.buttonContainer}>
-            <Button style={LoginScreenStyle.button} marginY={3} colorScheme="primary" onPress={handleLogin}>
-              <Text style={LoginScreenStyle.buttonText}> {t("LOGIN_SCREEN.FORM.SIGN_IN")} </Text>
-            </Button>
-
-            <Button bgColor={colors["primary-dark"]} style={LoginScreenStyle.button} onPress={handleSignUp}>
-              <Text style={LoginScreenStyle.buttonText}>{t("LOGIN_SCREEN.FORM.SIGN_UP")}</Text>
-            </Button>
+            {login ? (
+              <>
+                <Button style={LoginScreenStyle.button} marginY={3} colorScheme="primary" onPress={handleLogin}>
+                  <Text style={LoginScreenStyle.buttonText}> {t("LOGIN_SCREEN.FORM.SIGN_IN")} </Text>
+                </Button>
+                <Link marginTop={3} onPress={() => setLogin(!login)}>
+                  <Text color={colors["primary-dark"]}>{t("LOGIN_SCREEN.FORM.SIGN_UP")}</Text>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Button bgColor={colors["primary-dark"]} style={LoginScreenStyle.button} onPress={handleSignUp}>
+                  <Text style={LoginScreenStyle.buttonText}>{t("LOGIN_SCREEN.FORM.SIGN_UP")}</Text>
+                </Button>
+                <Link marginTop={3} onPress={() => setLogin(!login)}>
+                  <Text color={colors["primary-dark"]}>{t("LOGIN_SCREEN.FORM.SIGN_IN")}</Text>
+                </Link>
+              </>
+            )}
           </View>
         </Box>
         <Box position="absolute" bottom={5}>
