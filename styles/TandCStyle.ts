a4c30d0963729be1d26bbd64c94b4f652fd919d5
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-03-20 17:49:24 +0100

    Update login form container styling

--- styles/TandCStyle.ts
+++ styles/TandCStyle.ts
@@ -1,3 +1,5 @@
 import { StyleSheet } from "react-native";
 
-export const TandCStyle = StyleSheet.create({ image: { alignSelf: "center", height: 50, resizeMode: "contain", width: 100 } });
+export const TandCStyle = StyleSheet.create({
+  image: { alignSelf: "center", height: 50, resizeMode: "contain", width: 100 },
+});
