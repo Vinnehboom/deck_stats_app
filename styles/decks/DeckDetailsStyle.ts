a63b34f0686970c06fab974876cf1e9b4b9937f4
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-03-21 19:58:01 +0100

    Update DeckDetails page with form and match history
    TODO: add paginated complete match history

---
+++ styles/decks/DeckDetailsStyle.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "../../utils/colors";
+
+export const DeckDetailsStyle = StyleSheet.create({
+  formTitle: { fontSize: 18, fontWeight: "bold", marginVertical: 20, textAlign: "center" },
+  historyTitle: { fontSize: 20, fontWeight: "bold", marginVertical: 12, textAlign: "center" },
+  matchRecordFormContainer: {
+    backgroundColor: colors.light,
+    borderRadius: 10,
+    height: "auto",
+    marginBottom: 40,
+    marginHorizontal: "10%",
+    minWidth: "80%",
+  },
+  optionsContainer: {
+    alignSelf: "center",
+    justifyContent: "space-around",
+    marginVertical: 12,
+    paddingBottom: 16,
+    width: "75%",
+  },
+  optionsTitle: { fontSize: 20, fontWeight: "bold", marginVertical: 2, textAlign: "center" },
+});
