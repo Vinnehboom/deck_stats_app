ff4d8d8992de4fe0ca796c65ee974b405c04b232
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-03-22 13:32:00 +0100

    Flatten archetypes before querying the search string
    Still a WIP, but now variants can more easily be found with the search box

--- helpers/archetypes.ts
+++ helpers/archetypes.ts
@@ -1,25 +1,25 @@
 import { Archetype, ArchetypeBase } from "../types";
 
 export const transformArchetypes = (archetypes: Archetype[], generations: number[], query: string) => {
-  let filteredTypes = archetypes.filter(archetype => generations.includes(archetype.generation));
+  let filteredTypes: Archetype[] = archetypes
+    .filter(archetype => generations.includes(archetype.generation))
+    .sort((a, b) => (a.generation > b.generation ? 1 : -1));
   if (query.length > 0) {
-    filteredTypes = filteredTypes
-      .filter(
-        archetype =>
-          archetype.identifier.split("-").join(" ").toLowerCase().match(query.toLowerCase()) ||
-          archetype.name.toLowerCase().includes(query.toLowerCase())
-      )
-      .sort((a, b) => (a.generation > b.generation ? 1 : -1));
+    const flattenedTypes: ArchetypeBase[] = filteredTypes
+      .sort((a, b) => (a.generation === b.generation && a.priority < b.priority ? 1 : -1))
+      .reduce((acc: ArchetypeBase[], archetype: Archetype) => {
+        const variants = archetype.variants;
+        variants.forEach(variant => (variant.icons = [...archetype.icons, variant.icon]));
+        return [...acc, archetype, ...variants];
+      }, []);
+    filteredTypes = flattenedTypes.filter(
+      archetype =>
+        archetype.identifier.split("-").join(" ").toLowerCase().match(query.toLowerCase()) ||
+        archetype.name.toLowerCase().match(query.toLowerCase())
+    );
   }
-  const flattenedTypes: ArchetypeBase[] = filteredTypes
-    .sort((a, b) => (a.generation === b.generation && a.priority < b.priority ? 1 : -1))
-    .reduce((acc: ArchetypeBase[], archetype: Archetype) => {
-      const variants = archetype.variants;
-      variants.forEach(variant => (variant.icons = [...archetype.icons, variant.icon]));
-      return [...acc, archetype, ...variants];
-    }, []);
 
-  return flattenedTypes;
+  return filteredTypes;
 };
 
 export const transformIdentifier = (identifier: string) => {
