bbde2b89beaf2354b8163d54b7c8a270918e4803
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-04-15 14:24:12 +0200

    Update DeckItem styling

--- src/styles/decks/DeckItemStyle.ts
+++ src/styles/decks/DeckItemStyle.ts
@@ -4,24 +4,30 @@ import { Colors, Spacing, Typography } from "../variables";
 
 export const DeckItemStyle = StyleSheet.create({
   container: {
-    borderRadius: 5,
+    backgroundColor: Colors.primary,
+    borderRadius: 15,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
     margin: Spacing.xs,
-    padding: Spacing.xxs,
+    padding: Spacing.xs,
+    shadowColor: Colors.primary,
+    shadowOffset: { height: 2, width: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 0,
   },
   deckName: {
     alignItems: "center",
+    color: Colors.white,
     fontSize: Typography.fontSizes.lg,
     fontWeight: "500",
     paddingLeft: Spacing.md,
     paddingTop: Spacing.sm,
   },
   showLink: {
-    color: Colors["primary-dark"],
     fontSize: Typography.fontSizes.lg,
-    paddingTop: Spacing.sm,
+    marginTop: Spacing.sm,
     textDecorationLine: "underline",
+    top: Spacing.xxs,
   },
 });
