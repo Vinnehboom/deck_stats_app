b9bd2174a67b429f9186db1d890d385f05c14964
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-05-23 18:50:38 +0100

    Simplify archetype sort to generation
    This will be the most user friendly until we have time to look
    at better sorting

--- src/helpers/archetypes.ts
+++ src/helpers/archetypes.ts
@@ -3,15 +3,13 @@ import { Archetype, ArchetypeBase } from "../types";
 export const transformArchetypes = (archetypes: Archetype[], generations: number[], query: string) => {
   let filteredTypes: Archetype[] = archetypes
     .filter(archetype => generations.includes(archetype.generation))
-    .sort((a, b) => (a.generation > b.generation ? 1 : -1));
+    .sort((a, b) => (a.generation > b.generation ? -1 : 1));
   if (query.length > 0) {
-    const flattenedTypes: ArchetypeBase[] = filteredTypes
-      .sort((a, b) => (a.generation === b.generation && a.priority < b.priority ? 1 : -1))
-      .reduce((acc: ArchetypeBase[], archetype: Archetype) => {
-        const variants = archetype.variants;
-        variants.forEach(variant => (variant.icons = [...archetype.icons, variant.icon]));
-        return [...acc, archetype, ...variants];
-      }, []);
+    const flattenedTypes: ArchetypeBase[] = filteredTypes.reduce((acc: ArchetypeBase[], archetype: Archetype) => {
+      const variants = archetype.variants;
+      variants.forEach(variant => (variant.icons = [...archetype.icons, variant.icon]));
+      return [...acc, archetype, ...variants];
+    }, []);
     filteredTypes = flattenedTypes.filter(
       archetype =>
         archetype.identifier.split("-").join(" ").toLowerCase().match(query.toLowerCase()) ||
