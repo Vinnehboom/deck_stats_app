0c64b2dd5fafc031871ed2f85409f83e1a012e49
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-06-21 15:31:50 +0200

    Allow users to change between the available languages
    Consideration for the future is to allow a user to save a preferred
    language

--- src/contexts/TranslationContext.ts
+++ src/contexts/TranslationContext.ts
@@ -1,7 +1,10 @@
 import { createContext } from "react";
 
+import { defaultLocale } from "../helpers/locales";
+
 type TranslationContextType = {
   t: (key: string) => string;
+  locale: string;
 };
 
 export const TranslationContext = createContext<TranslationContextType>({
@@ -11,4 +14,5 @@ export const TranslationContext = createContext<TranslationContextType>({
       message: `t() should be overridden by a useState setter or similar in the component that uses the Auth Provider.`,
     };
   },
+  locale: defaultLocale,
 });
