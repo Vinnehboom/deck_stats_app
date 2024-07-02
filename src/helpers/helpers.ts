ef354b64d1a0e112979a4b6f00aa1ae00118746b
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-07-02 16:43:44 +0100

    Add GLC archetypes & icons

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,3 +1,7 @@
 export const truncate = (string: string) => {
   return string.length > 35 ? `${string.substring(0, 35)}...` : string;
 };
+
+export const capitalizeFirstLetter = (str: string) => {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+};
