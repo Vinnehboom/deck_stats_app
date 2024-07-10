05b80bf400d023a9c21c377f76fdd70b688fcd4f
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-07-10 13:42:56 +0100

    Improve alignment of export page match rows

--- src/components/matchRecords/Coinflip.tsx
+++ src/components/matchRecords/Coinflip.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import { HStack, Image } from "native-base";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faXmark, faCheck } from "@fortawesome/free-solid-svg-icons";
-import { DimensionValue } from "react-native";
+import { DimensionValue, StyleProp, ViewStyle } from "react-native";
 
 import { MatchRecordListItemStyle } from "../../styles/matchRecords/MatchRecordListItemStyle";
 MatchRecordListItemStyle;
 
-export const Coinflip = ({ won, paddingTop }: { won: boolean; paddingTop?: DimensionValue }) => {
+export const Coinflip = ({
+  won,
+  paddingTop,
+  containerStyle,
+}: {
+  won: boolean;
+  paddingTop?: DimensionValue;
+  containerStyle?: StyleProp<ViewStyle>;
+}) => {
   return (
-    <HStack space={1} alignItems="center" paddingTop={paddingTop}>
+    <HStack space={1} alignItems="center" style={containerStyle} paddingTop={paddingTop}>
       <Image style={MatchRecordListItemStyle.coinflip} alt="coin-flip" source={require("../../assets/images/coin_icon.png")} />
       <FontAwesomeIcon icon={won ? faCheck : faXmark} />
     </HStack>
