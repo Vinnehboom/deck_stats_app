1caa08e0c571e5011d246fa62199da12bb0cebfa
Dennis Havermans <dennishvrmans@gmail.com>
2024-08-25 21:56:36 +0200

    Add expo to the codebase

--- android/app/src/main/java/com/deckstatstcg/MainActivity.kt
+++ android/app/src/main/java/com/deckstatstcg/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.deckstatstcg
-
+import expo.modules.ReactActivityDelegateWrapper
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -26,5 +26,5 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+       ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
 }
